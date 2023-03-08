223f43a2e0013614e112be93042d467a36d8f5b8
Olafur Waage <olafurw@gmail.com>
2023-03-08 20:43:22 +0100

    Redid the csv files and adding a small sample file.

--- song-names/parse.py
+++ song-names/parse.py
@@ -1,3 +1,7 @@
 from songdb import load_songs
+import csv
 
-songs = load_songs('tracks_features.csv')
+songs = load_songs('some_tracks.csv')
+
+for song in songs:
+    print(song)
--- song-names/songdb.py
+++ song-names/songdb.py
@@ -1,28 +1,14 @@
 import csv
-from dataclasses import dataclass, field
-from typing import List
-from datetime import datetime, time
-
-def release_to_date(release):
-    if len(release) == 4 and release != '0000':
-        return datetime.strptime(release, '%Y')
-    elif len(release) == 7:
-        return datetime.strptime(release, '%Y-%m')
-    elif len(release) == 10:
-        return datetime.strptime(release, '%Y-%m-%d')
-    else:
-        return datetime.strptime('1970-01-01', '%Y-%m-%d')
+import sys
+import random
+from dataclasses import dataclass
 
 @dataclass
 class Song:
-    id: str = ''
     name: str = ''
     album: str = ''
-    album_id: str = ''
-    artists: List[str] = field(default_factory=list)
-    artists_ids: List[str] = field(default_factory=list)
+    artists: str = ''
     track_number: int = 0
-    disc_number: int = 0
     explicit: bool = False
     danceability: float = 0.0
     energy: float = 0.0
@@ -37,20 +23,18 @@ class Song:
     tempo: float = 0.0
     duration_ms: int = 0
     time_signature: float = 0.0
-    year: int = 0
-    release_date: datetime = datetime(1970, 1, 1)
+    release_date: str = ''
 
 def load_songs(filename):
     songs = []
     with open(filename, encoding='utf-8') as file:
-        next(file) # skips the first row
-
         reader = csv.reader(file)
         for song in reader:
-            s = Song(song[0], song[1], song[2], song[3], song[4], song[5],
-                    int(song[6]), int(song[7]), bool(song[8]), float(song[9]), float(song[10]),
-                    int(song[11]), float(song[12]), int(song[13]), float(song[14]),
-                    float(song[15]), float(song[16]), float(song[17]), float(song[18]),
-                    float(song[19]), int(song[20]), float(song[21]), int(song[22]), release_to_date(song[23]))
+            s = Song(song[0], song[1], song[2],
+                    int(song[3]), bool(song[4]), round(float(song[5]), 4), round(float(song[6]), 4),
+                    int(song[7]), round(float(song[8]), 4), int(song[9]), round(float(song[10]), 4),
+                    round(float(song[11]), 4), round(float(song[12]), 4), round(float(song[13]), 4), 
+                    round(float(song[14]), 4), round(float(song[15]), 4), 
+                    int(song[16]), round(float(song[17]), 4), song[18])
             songs.append(s)
     return songs
